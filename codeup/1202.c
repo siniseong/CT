c0e5ac40bae598dc74bab2b523e8e893debc545d
huise0ng <shinhuiseong07@gmail.com>
2023-05-17 23:44:32 +0900

    codeup-1202

---
+++ codeup/1202.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+ 
+int main (void)
+{
+    
+    int num;
+    scanf("%d",&num);
+    
+    if(num >= 90){printf("A");}
+    else if(num >= 80){printf("B");}
+    else if(num >= 70){printf("C");}
+    else if(num >= 60){printf("D");}
+    else{printf("F");}
+ 
+    return 0;
+}
