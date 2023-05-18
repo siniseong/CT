ac75f5e99a6a2204fae8599812d79f48c152d9c9
huise0ng <shinhuiseong07@gmail.com>
2023-05-18 17:54:57 +0900

    codeup-1163

---
+++ codeup/1163.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+ 
+int main (void)
+{
+    
+    int a,b,c, d;
+    scanf("%d %d %d ",&a , &b, &c);
+    d = a + b + c; //사주는 년 + 월+일
+ 
+    d %=1000;
+ 
+ 
+ 
+    d /= 100;
+    if( d%2 ){printf("그럭저럭");}
+   
+    else {printf("대박");}
+    
+ 
+    return 0;
+}
