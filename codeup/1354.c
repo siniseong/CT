3f8e8f6918b7d7a351a424c007f4ff486592c079
huise0ng <shinhuiseong07@gmail.com>
2023-12-04 11:54:39 +0900

    ADD :: 1354

---
+++ codeup/1354.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ 
+ 
+int main(){
+ 
+    int num;
+    scanf("%d", &num);
+    
+    for(int i = 1; i<= num; i++){
+        for(int j = i; j <=num; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+ 
+    return 0;
+}
+ 
