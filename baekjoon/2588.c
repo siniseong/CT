4bc2d69b8243eba51c6a50837a8c0b34313186fb
huise0ng <shinhuiseong07@gmail.com>
2023-12-06 10:27:28 +0900

    ADD :: 2588

---
+++ baekjoon/2588.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    printf("%d\n", a * (b % 10));
+    printf("%d\n", a * (b % 100 / 10));
+    printf("%d\n", a * (b / 100));
+    printf("%d\n", a * b);
+
+    return 0;
+}
